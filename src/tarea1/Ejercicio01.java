740ddb8fd41542d88bf29e94e4b1f3083246c1a0
Illu13 <isaacdelgado442@gmail.com>
2022-12-12 14:49:20 +0100

    addition and substraction 1

--- src/tarea1/Ejercicio01.java
+++ src/tarea1/Ejercicio01.java
@@ -3,10 +3,24 @@ package tarea1;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
+
+		int opcion = Utils
+				.obtenerEnteroPorJOptionPaneConDescripcion("1.-Suma" + "\n2.-Resta" + "\n3.-Multiplicación" + "\nIntroduzca una opción.");
+		int numero1 = Utils.obtenerEnteroPorJOptionPaneConDescripcion("Introduzca el primero número");
+		int numero2 = Utils.obtenerEnteroPorJOptionPaneConDescripcion("Introduzca el segundo número");
+
+		switch (opcion) {
 		
-		int opcion = Utils.obtenerEnteroPorJOptionPaneConDescripcion("1.-Suma" + 
-				"\n2.-Resta" + 
-				"\nIntroduzca una opción.");
+		
+		case 1:
+			System.out.println(numero1 + numero2);
+			break;
+		case 2:
+			System.out.println(numero1 - numero2);
+			break;
+		
+
+		}
 
 	}
 
