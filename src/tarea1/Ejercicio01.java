5124f42b05c0848e7f4d526c72a7e3d999e3d83e
Illu13 <isaacdelgado442@gmail.com>
2022-12-12 14:25:41 +0100

    first version

---
+++ src/tarea1/Ejercicio01.java
@@ -0,0 +1,13 @@
+package tarea1;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		int opcion = Utils.obtenerEnteroPorJOptionPaneConDescripcion("1.-Suma" + 
+				"\n2.-Resta" + 
+				"\nIntroduzca una opción.");
+
+	}
+
+}
