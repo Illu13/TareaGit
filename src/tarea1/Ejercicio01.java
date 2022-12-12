fe771494414e0e795b8bdc4597280bc902e07e40
Illu13 <isaacdelgado442@gmail.com>
2022-12-12 14:31:19 +0100

    final version

--- src/tarea1/Ejercicio01.java
+++ src/tarea1/Ejercicio01.java
@@ -5,7 +5,7 @@ public class Ejercicio01 {
 	public static void main(String[] args) {
 
 		int opcion = Utils
-				.obtenerEnteroPorJOptionPaneConDescripcion("1.-Suma" + "\n2.-Resta" + "\nIntroduzca una opción.");
+				.obtenerEnteroPorJOptionPaneConDescripcion("1.-Suma" + "\n2.-Resta" + "\n3.-Multiplicación" + "\nIntroduzca una opción.");
 		int numero1 = Utils.obtenerEnteroPorJOptionPaneConDescripcion("Introduzca el primer número");
 		int numero2 = Utils.obtenerEnteroPorJOptionPaneConDescripcion("Introduzca el segundo número");
 
@@ -17,6 +17,10 @@ public class Ejercicio01 {
 			break;
 		case 2:
 			System.out.println(numero1 - numero2);
+			break;
+		case 3:
+			System.out.println(numero1 * numero2);
+			break;
 
 		}
 
